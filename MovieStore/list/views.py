f3f93a0d6defbee5bd3d8575f97cfb53c9ba78fd
David Diaz <daviddiaz031706@gmail.com>
2025-01-24 18:55:39 -0500

    Added list app

--- MovieStore/list/views.py
+++ MovieStore/list/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.template import loader
-from django.http import HttpResponse
+from django.shortcuts import render
+from firebase_admin import firestore
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Replace with the path to your service account key file
+cred = credentials.Certificate("C:\\Users\\david\\PycharmProjects\\CS2340-MovieStore\\MovieStore\\service_key.json")
+firebase_admin.initialize_app(cred)
+
+
+
 
 def index(request):
-    template = loader.get_template('list/listview.html')
-    return HttpResponse(template.render())
+    db = firestore.client()
+    MOVIES_REF = db.collection('Movies')
+    docs = MOVIES_REF.stream()
+    movies = []
+    i = 0
+    for doc in docs:
+        movies.append(doc.to_dict())
+    return render(request, 'list/listview.html', {'movies': movies})
