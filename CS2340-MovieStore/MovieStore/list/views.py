6c95a5497719f9ec4158132634fbc4d098e0f3c0
DavidDiaz0317 <DAVIDDIAZ031706@GMAIL.COM>
2025-01-24 19:00:50 -0500

    Update views.py
    Updated list view to support database

--- CS2340-MovieStore/MovieStore/list/views.py
+++ CS2340-MovieStore/MovieStore/list/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.template import loader
-from django.http import HttpResponse
+from django.shortcuts import render
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+#Initialize Database
+cred = credentials.Certificate("C:\\Users\\david\\PycharmProjects\\CS2340-MovieStore\\MovieStore\\service_key.json")
+firebase_admin.initialize_app(cred)
 
 def index(request):
-    template = loader.get_template('list/listview.html')
-    return HttpResponse(template.render())
+
+    #Open Database
+    db = firestore.client()
+
+    #Store movies from database in a dictionary
+    MOVIES_REF = db.collection('Movies')
+    docs = MOVIES_REF.stream()
+    movies = []
+    for doc in docs:
+        movies.append(doc.to_dict())
+
+    #Render list site
+    return render(request, 'list/listview.html', {'movies': movies})
