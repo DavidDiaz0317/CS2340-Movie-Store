34d75300f69cb0b9292cbf5a2b2bb2fc7eeaec1d
Avril <femiprattavril@gmail.com>
2025-01-28 01:55:25 -0500

    In this commit, I created the Home page, and the About page. I also added the header (which also has the nav bar and toggle option) and footer section.These also use bootstrap, in addition to CSS, and HTML.

---
+++ CS2340-MovieStore/MovieStore/home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    template_data = {}
+    template_data['title'] = 'Movie Store'
+    return render(request, 'home/home.html', {'template_data': template_data})
+
+def about(request):
+    return render(request, 'home/about.html')
