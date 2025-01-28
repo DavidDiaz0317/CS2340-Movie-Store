5c9623ce356763cfa40fcd1cb02d17fcb94aa65b
David Diaz <daviddiaz031706@gmail.com>
2025-01-28 11:29:04 -0500

    Added title, year, and photo to movie list

--- MovieStore/list/views.py
+++ MovieStore/list/views.py
@@ -1,11 +1,9 @@
 # Create your views here.
 from django.shortcuts import render
-from firebase_admin import firestore
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Replace with the path to your service account key file
 cred = credentials.Certificate("C:\\Users\\david\\PycharmProjects\\CS2340-MovieStore\\MovieStore\\service_key.json")
 firebase_admin.initialize_app(cred)
 
@@ -15,9 +13,10 @@ firebase_admin.initialize_app(cred)
 def index(request):
     db = firestore.client()
     MOVIES_REF = db.collection('Movies')
+    genres = ["Thriller", "Adventure", "Comedy", "Action", "Crime", "Drama", "Romance", "Sci-Fi", "War", "Animation", "History", "Family", "Biography", "Western", "Fantasy"]
     docs = MOVIES_REF.stream()
     movies = []
     i = 0
     for doc in docs:
         movies.append(doc.to_dict())
-    return render(request, 'list/listview.html', {'movies': movies})
+    return render(request, 'list/listview.html', {'movies': movies, 'genres': genres})
