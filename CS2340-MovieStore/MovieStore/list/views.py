ab3cfd9718f0340c9ed96cd1e0ad49732bc9a6fe
DavidDiaz0317 <DAVIDDIAZ031706@GMAIL.COM>
2025-01-28 11:54:52 -0500

    Update views.py

--- CS2340-MovieStore/MovieStore/list/views.py
+++ CS2340-MovieStore/MovieStore/list/views.py
@@ -4,21 +4,19 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-#Initialize Database
 cred = credentials.Certificate("C:\\Users\\david\\PycharmProjects\\CS2340-MovieStore\\MovieStore\\service_key.json")
 firebase_admin.initialize_app(cred)
 
+
+
+
 def index(request):
-
-    #Open Database
     db = firestore.client()
-
-    #Store movies from database in a dictionary
     MOVIES_REF = db.collection('Movies')
+    genres = ["Thriller", "Adventure", "Comedy", "Action", "Crime", "Drama", "Romance", "Sci-Fi", "War", "Animation", "History", "Family", "Biography", "Western", "Fantasy"]
     docs = MOVIES_REF.stream()
     movies = []
+    i = 0
     for doc in docs:
         movies.append(doc.to_dict())
-
-    #Render list site
-    return render(request, 'list/listview.html', {'movies': movies})
+    return render(request, 'list/listview.html', {'movies': movies, 'genres': genres})
