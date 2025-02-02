d9e3d49828f29fa6c8a2b256615c53cc7be48a6e
Leah Injaty <leah@Leahs-MacBook-Pro.local>
2025-02-02 13:39:35 -0500

    edits to home-page

--- CS2340-MovieStore/MovieStore/MovieStore/settings.py
+++ CS2340-MovieStore/MovieStore/MovieStore/settings.py
@@ -123,16 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'MovieStore/static',
-]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
