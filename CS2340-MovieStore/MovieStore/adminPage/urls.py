69f0024e85e72398bf499f65083a5acc533c2cf8
Avril <femiprattavril@gmail.com>
2025-02-02 22:11:07 -0500

    modified admin_dashboard, admin_user and debugged unique email

--- CS2340-MovieStore/MovieStore/adminPage/urls.py
+++ CS2340-MovieStore/MovieStore/adminPage/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("dashboard/", views.admin_home, name="admin_home"),
+    path("movies/", views.admin_movies, name="admin_movies"),
+    path("reviews/", views.admin_reviews, name="admin_reviews"),
 
-    path("movies/", views.movies, name="movies"),
-    path("users/", views.users, name="users"),
-    path("reviews/", views.reviews, name="reviews"),
+    #user management
+    path("users/", views.admin_users, name="admin_users"),
+    path('create/', views.create_user, name="create_user"),
+    path('edit/<int:user_id>/', views.edit_user, name="edit_user"),
+    path('delete/<int:user_id>/', views.delete_user, name="delete_user"),
 ]
