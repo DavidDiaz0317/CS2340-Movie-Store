d9f8b450a7684b4ed31e2bbca531756e005de431
Leah Injaty <leah@Leahs-MacBook-Pro.local>
2025-02-11 01:59:28 -0500

    added UI to create reviews

--- CS2340-MovieStore/MovieStore/list/models.py
+++ CS2340-MovieStore/MovieStore/list/models.py
@@ -1,6 +1,7 @@
 import json
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -26,10 +27,15 @@ class Movie(models.Model):
         return self.Title
 
 class Review(models.Model):
+    id = models.AutoField(primary_key=True)
 
-    User = models.CharField(max_length=200)
+    comment = models.CharField(max_length=255)
 
-    Body = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.User
--- CS2340-MovieStore/MovieStore/list/views.py
+++ CS2340-MovieStore/MovieStore/list/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.shortcuts import render
-from .models import Movie
+from .models import Movie, Review
 from django.shortcuts import get_object_or_404, redirect
 
 #Initialize Database
