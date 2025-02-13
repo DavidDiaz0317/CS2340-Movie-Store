2c539fda97eec99531ec58daaccc1620e100f31e
Avril <femiprattavril@gmail.com>
2025-02-13 14:10:26 -0500

    deleting movie model from list

--- CS2340-MovieStore/MovieStore/list/admin.py
+++ CS2340-MovieStore/MovieStore/list/admin.py
@@ -6,10 +6,8 @@ from django.contrib import admin
 
 from .models import Movie
 from .models import Review
-from .models import Order
 
 admin.site.register(Movie)
 admin.site.register(Review)
-admin.site.register(Order)
 
 
